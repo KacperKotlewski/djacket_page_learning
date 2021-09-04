429192e463ec87c99692573a75c250997929c1a3
Kacper Kotlewski <kotelwski.kacper@gmail.com>
2021-09-04 15:21:39 +0200

    added circle loading to home page also

--- vue_djacket/src/views/Home.vue
+++ vue_djacket/src/views/Home.vue
@@ -50,8 +50,10 @@ export default {
     this.getLatestProducts()
   },
   methods: {
-    getLatestProducts() {
-      axios
+    async getLatestProducts() {
+      this.$store.commit("setIsLoading", true)
+
+      await axios
           .get('/api/v1/latest-products/')
           .then(response => {
             this.latestProducts = response.data
@@ -59,6 +61,8 @@ export default {
           .catch(error => {
             console.log(error)
           })
+      
+      this.$store.commit("setIsLoading", false)
     }
   }
 }
